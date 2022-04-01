c8db9d46b98b2e606ab83034f64e758850b10102
Luís Ferreira <contact@lsferreira.net>
2022-04-01 16:21:31 +0100

    chore: translate from javascript to typescript
    Signed-off-by: Luís Ferreira <contact@lsferreira.net>

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,25 @@ const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
+    entry: './src/index.ts',
     target: 'node',
-    entry: './index.js',
+    devtool: 'source-map',
     mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
     },
     optimization: {
         minimize: true,
